ef1e7508aef394bac20f35239bc942b39b8b351f
Adam O'Neil <adamosoftware@gmail.com>
2019-11-24 12:30:30 -0500

    added ITestableQuery

---
+++ Dapper.QX/Interfaces/ITestableQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace Dapper.QX.Interfaces
+{
+    /// <summary>
+    /// Implement this on your Query types to make it easy to unit test your queries.
+    /// Use <see cref="Query{TResult}.TestExecuteHelper(IDbConnection)"/> in your interface implementation
+    /// </summary>
+    public interface ITestableQuery
+    {
+        IEnumerable<dynamic> TestExecute(IDbConnection connection);
+        IEnumerable<ITestableQuery> GetTestCases();
+    }
+}
--- Dapper.QX/Query.cs
+++ Dapper.QX/Query.cs
@@ -1,4 +1,8 @@
-﻿namespace Dapper.QX
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Dapper.QX
 {
     public class Query<TResult>
     {
@@ -12,5 +16,20 @@
         public string ResolvedSql { get; private set; }
         public DynamicParameters Parameters { get; private set; }
 
+        /// <summary>
+        /// Intended for implementing <see cref="Interfaces.ITestableQuery"/> for unit testing, not intended for use on its own
+        /// </summary>
+        public IEnumerable<dynamic> TestExecuteHelper(IDbConnection connection)
+        {
+            try
+            {
+                ResolvedSql = QueryUtil.ResolveSql(Sql, this, out DynamicParameters queryParams);
+                return connection.Query(ResolvedSql, queryParams);
+            }
+            catch (Exception exc)
+            {
+                throw new Exception($"Query {GetType().Name} failed: {exc.Message}", exc);
+            }
+        }
     }
 }
