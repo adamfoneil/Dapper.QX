a0b6ba3344fabaf58782453f59a004bc73b6e975
Adam O'Neil <adamosoftware@gmail.com>
2020-11-24 14:05:17 -0500

    v1.0.20 PageableQuery.GetMetricsAsync

--- Dapper.QX/PageableQuery.cs
+++ Dapper.QX/PageableQuery.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper.QX.Attributes;
+using System;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,14 +20,46 @@ namespace Dapper.QX
 
         protected const int DefaultPageSize = 30;
 
+        public async Task<int> GetPageCountAsync(Func<IDbConnection> getConnection, int newPageSize = 0)
+        {
+            using (var cn = getConnection.Invoke())
+            {
+                return await GetPageCountAsync(cn, newPageSize);
+            }
+        }
+
         public async Task<int> GetPageCountAsync(IDbConnection connection, int newPageSize = 0)
+        {
+            var metrics = await GetMetricsAsync(connection, newPageSize);
+            return metrics.Pages;
+        }
+
+        public async Task<ResultMetrics> GetMetricsAsync(Func<IDbConnection> getConnection, int newPageSize = 0)
+        {
+            using (var cn = getConnection.Invoke())
+            {
+                return await GetMetricsAsync(cn, newPageSize);
+            }
+        }
+
+        public async Task<ResultMetrics> GetMetricsAsync(IDbConnection connection, int newPageSize = 0)
         {
             int pageSize = (newPageSize > 0) ? newPageSize : DefaultPageSize;
 
             this.Page = null;
             var results = await ExecuteAsync(connection, newPageSize: newPageSize);
             int count = results.Count();
-            return (count / pageSize) + (((count % pageSize) > 0) ? 1 : 0);
+            return new ResultMetrics()
+            {
+                Rows = count,
+                Pages = (count / pageSize) + (((count % pageSize) > 0) ? 1 : 0)
+            };
+        }
+
+        public class ResultMetrics
+        {
+            public int Pages { get; set; }
+            public int Rows { get; set; }
         }
     }
 }
