66027c8dc2e29f848401b1387cbfc1f81cf4bbc2
Adam Oneil <adamsoftware@gmail.com>
2022-03-31 20:21:07 -0400

    started on ICriteriaDescription

---
+++ Dapper.QX/Interfaces/ICriteriaDescription.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Dapper.QX.Interfaces
+{
+    /// <summary>
+    /// implement this on queries when you want to generate a plain-English description 
+    /// of the criteria in effect on a query
+    /// </summary>
+    public interface ICriteriaDescription
+    {
+        Task<List<string>> GetCriteriaTermsAsync(IDbConnection connection);
+    }
+}
