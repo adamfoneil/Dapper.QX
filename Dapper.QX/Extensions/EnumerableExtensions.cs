faf5f6ab0c8bd83187742d02e98c2573a6b19fdc
Adam Oneil <adamsoftware@gmail.com>
2022-04-06 06:40:58 -0400

    v1.0.31 simplified TVP support

--- Dapper.QX/Extensions/EnumerableExtensions.cs
+++ Dapper.QX/Extensions/EnumerableExtensions.cs
@@ -49,39 +49,9 @@ namespace Dapper.QX.Extensions
             return dataTable;
         }
         
-        /// <summary>
-        /// thanks to https://www.codeproject.com/Articles/835519/Passing-Table-Valued-Parameters-with-Dapper
-        /// </summary>
-        public static SqlMapper.ICustomQueryParameter AsTableValuedParameter<T>(this IEnumerable<T> enumerable, string typeName, params string[] columnNames)
+        public static SqlMapper.ICustomQueryParameter AsTableValuedParameter<T>(this IEnumerable<T> enumerable, string typeName)
         {
-            var dataTable = new DataTable();
-
-            if (typeof(T).IsValueType || typeof(T).FullName.Equals("System.String"))
-            {
-                dataTable.Columns.Add(columnNames == null ? "NONAME" : columnNames.First(), typeof(T));
-                foreach (T obj in enumerable) dataTable.Rows.Add(obj);
-            }
-            else
-            {
-                PropertyInfo[] properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-                PropertyInfo[] readableProperties = properties.Where(w => w.CanRead).ToArray();
-
-                if (readableProperties.Length > 1 && columnNames == null)
-                {
-                    throw new ArgumentException("Ordered list of column names must be provided when TVP contains more than one column");
-                }
-
-                var createColumns = (columnNames ?? readableProperties.Select(s => s.Name)).ToArray();
-                foreach (string name in createColumns)
-                {
-                    dataTable.Columns.Add(name, readableProperties.Single(s => s.Name.Equals(name)).PropertyType);
-                }
-
-                foreach (T obj in enumerable)
-                {
-                    dataTable.Rows.Add(createColumns.Select(s => readableProperties.Single(s2 => s2.Name.Equals(s)).GetValue(obj)).ToArray());
-                }
-            }
+            var dataTable = enumerable.ToDataTable();
 
             return dataTable.AsTableValuedParameter(typeName);
         }
